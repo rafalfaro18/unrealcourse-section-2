b0cd775d83a7f8923f0684370e55ded3a37c9c97
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-04-01 20:46:34 -0600

    ask for user input and repeat it

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -6,9 +7,13 @@ int main() {
 	constexpr int WORLD_LENGHT = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORLD_LENGHT << " letter isogram I'm thinking off?\n";
+
 	//get a guess from the player
+	string Guess = "";
+	cin >> Guess;
 
 	//repeat the guess back to them
+	cout << Guess << "\n";
 
 	return 0;
 }
