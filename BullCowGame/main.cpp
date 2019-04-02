a03c5718be775336c488b04ef2245514f08db19d
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-04-01 20:42:40 -0600

    adds commented pseudo code

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main() {
+	//introduce the game
 	constexpr int WORLD_LENGHT = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game."<<endl;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORLD_LENGHT << " letter isogram I'm thinking off?\n";
+	//get a guess from the player
+
+	//repeat the guess back to them
+
 	return 0;
 }
