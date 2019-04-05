d1645065dd3f7535d5fdae530316d243d674ceff
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-04-04 21:27:44 -0600

    refactor

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -49,18 +49,15 @@ void FBullCowGame::Reset()
 // receives a VALID guess, increments turn, and returns count
 FBullCowCount FBullCowGame::SubmitValidGuess(FString Guess)
 {
-	// increment the turn number
 	MyCurrentTry++;
-
-	// setup a return variable
 	FBullCowCount BullCowCount;
+	int32 WordLength = MyHiddenWord.length(); // assuming same length as guess
 
-	// loop all letters in the guess
-	int32 HiddenWordLength = MyHiddenWord.length();
+	// loop all letters in the hidden word
 
-	for (int32 MHWChar = 0; MHWChar < HiddenWordLength; MHWChar++) {
-		// compare letters against the hidden word
-		for (int32 GChar = 0; GChar < HiddenWordLength; GChar++) {
+	for (int32 MHWChar = 0; MHWChar < WordLength; MHWChar++) {
+		// compare letters against the guess
+		for (int32 GChar = 0; GChar < WordLength; GChar++) {
 			// if they match then
 			if (Guess[GChar] == MyHiddenWord[MHWChar]) {
 				// if they're in the same place
